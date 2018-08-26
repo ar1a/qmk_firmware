c750e48d1078833a87daeea558191fb135689f53
Aria <aria@ar1as.space>
2018-08-27 09:32:38 +1000

    Make initial mods to keymap
    hjkl arrow keys, CAPS = fn, FN+Q = caps, move home/pgup to right side
    left and right to account for replacing with hjkl

--- keyboards/v60_type_r/keymaps/aria/keymap.c
+++ keyboards/v60_type_r/keymaps/aria/keymap.c
@@ -19,6 +19,10 @@
 */
 #include QMK_KEYBOARD_H
 
+#define _______ KC_TRNS
+
+#define _FN 1
+
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   /* Keymap 0: Default Layer (Qwerty)
@@ -27,7 +31,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |-----------------------------------------------------------|
    * |Tab  |  Q|  W|  E|  R|  T|  Y|  U|  I|  O|  P|  [|  ]|    \|
    * |-----------------------------------------------------------|
-   * |Caps   |  A|  S|  D|  F|  G|  H|  J|  K|  L|  ;|  '|Return |
+   * |FN0    |  A|  S|  D|  F|  G|  H|  J|  K|  L|  ;|  '|Return |
    * |-----------------------------------------------------------|
    * |LShift  |  Z|  X|  C|  V|  B|  N|  M|  ,|  .|  /| RShift   |
    * |-----------------------------------------------------------|
@@ -37,17 +41,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT_60_ansi(
 	       KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC, \
 	       KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, \
-	       KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,  \
+	       MO(_FN), KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,  \
 	       KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,          KC_RSFT,   \
-	       KC_LCTL, KC_LGUI, KC_LALT,          KC_SPC,                                      MO(1),   KC_RGUI, KC_APP,  KC_RCTL),
+	       KC_LCTL, KC_LGUI, KC_LALT,          KC_SPC,                                      MO(_FN),   KC_RGUI, KC_APP,  KC_RCTL),
 
   /* Keymap 1: FN Layer
    * ,-----------------------------------------------------------.
    * |`  | F1| F2| F3| F4| F5| F6| F7| F8| F9|F10|F11|F12|  Del  |
    * |-----------------------------------------------------------|
-   * |     |   |Up |   |   |   |   |   |Prt|Scl|Pau|Up|   | Ins  |
+   * |     |Cap|Up |   |   |   |   |   |Prt|Scl|Pau|Up|   | Ins  |
    * |-----------------------------------------------------------|
-   * |      |Lft|Dwn|Rig|   |   |   |   |Hme|PgU|Lef|Rig|        |
+   * |      |Lft|Dwn|Rig|   |   |Lef|Dow|Up |Rig|Hom|PgU|        |
    * |-----------------------------------------------------------|
    * |        |   |   |   |   |VolD|VolU|Mut|End|PgD|Dwn|        |
    * |-----------------------------------------------------------|
@@ -56,13 +60,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    */
   [1] = LAYOUT_60_ansi(
 	       KC_GRV,    KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,     KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,   KC_F12,  KC_DEL, \
-	       KC_TRNS,   KC_TRNS,  KC_UP,    KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,   KC_TRNS, KC_PSCR, KC_SLCK, KC_PAUS, KC_UP,    KC_TRNS,          KC_INS, \
-	       KC_TRNS,   KC_LEFT,  KC_DOWN,  KC_RIGHT, KC_TRNS,  KC_TRNS,  KC_TRNS,   KC_TRNS, KC_HOME, KC_PGUP, KC_LEFT, KC_RIGHT,          KC_TRNS,          \
-	       KC_TRNS,   KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_VOLD,   KC_VOLU, KC_MUTE, KC_END,  KC_PGDN,  KC_DOWN,           KC_TRNS,  \
-	       KC_TRNS,   KC_TRNS,  KC_TRNS,            KC_TRNS,                                                           KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS),
+	       _______,   KC_CAPS,  KC_UP,    _______,  _______,  _______,  _______,   _______, KC_PSCR, KC_SLCK, KC_PAUS, KC_UP,    _______,          KC_INS, \
+	       _______,   KC_LEFT,  KC_DOWN,  KC_RIGHT, _______,  _______,  KC_LEFT,   KC_DOWN, KC_UP, KC_RIGHT, KC_HOME, KC_PGUP,          _______,          \
+	       _______,   _______,  _______,  _______,  _______,  KC_VOLD,   KC_VOLU, KC_MUTE, KC_END,  KC_PGDN,  KC_DOWN,           _______,  \
+	       _______,   _______,  _______,            _______,                                                           _______,  _______, _______, _______),
 
 };
-
 
 
 
