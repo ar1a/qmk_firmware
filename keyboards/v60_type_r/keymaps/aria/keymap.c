c5a3c7465fe065f2f3a6e763915e14948bf52e08
Aria <aria@ar1as.space>
2018-08-30 15:52:31 +1000

    Move vol controls/home end around for muscle mem

--- keyboards/v60_type_r/keymaps/aria/keymap.c
+++ keyboards/v60_type_r/keymaps/aria/keymap.c
@@ -54,9 +54,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * |-----------------------------------------------------------|
      * |     |   |   |   |   |   |   |   |   |   |PSc|SLk|Pau| Ins |
      * |-----------------------------------------------------------|
-     * |      |   |   |   |   |   | Lt| Dn| Up| Rt|Hom|PgU|        |
+     * |      |   |VDn|VUp|Mut|   | Lt| Dn| Up| Rt|   |PgU|        |
      * |-----------------------------------------------------------|
-     * |        |   |   |Cap|   |VDn|VUp|Mut|End|PgD|FN1|          |
+     * |        |   |   |Cap|   |   |Hom|End|   |PgD|FN1|          |
      * |-----------------------------------------------------------|
      * |    |    |    |                        |    |    |    |    |
      * `-----------------------------------------------------------'
@@ -65,8 +65,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_FN] = LAYOUT_60_ansi(
     KC_GRAVE,  KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,   KC_DEL, \
     _______,   _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  KC_PSCR,  KC_SLCK,  KC_PAUS,  KC_INS, \
-    _______,   _______,  _______,  _______,  _______,  _______,  KC_LEFT,  KC_DOWN,  KC_UP,    KC_RGHT,  KC_HOME,  KC_PGUP,  _______,          \
-    _______,   _______,  _______,  KC_CAPS,  _______,  KC_VOLD,  KC_VOLU,  KC_MUTE,  KC_END,   KC_PGDN,  TG(_AR),  _______,  \
+    _______,   _______,  KC_VOLD,  KC_VOLU,  KC_MUTE,  _______,  KC_LEFT,  KC_DOWN,  KC_UP,    KC_RGHT,  _______,  KC_PGUP,  _______,          \
+    _______,   _______,  _______,  KC_CAPS,  _______,  _______,  KC_HOME,  KC_END,   _______,  KC_PGDN,  TG(_AR),  _______,  \
     _______,   _______,  _______,  _______,  _______,  _______,  _______,  _______),
 
   /* Keymap 2: AR Layer
